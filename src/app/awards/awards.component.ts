eb9e9b2aec2583b0c9a05284fdbc1c3d119d2574
dkearsley89 <34358556+dkearsley89@users.noreply.github.com>
2022-07-31 22:22:34 +0930

    Updated to Angular v14.1 + Improved Mobile Views

--- src/app/awards/awards.component.ts
+++ src/app/awards/awards.component.ts
@@ -11,7 +11,9 @@ import { YearlyAwards } from '../model/model';
 export class AwardsComponent implements OnInit {
   yearlyAwardsData!: YearlyAwards;
   active: string = '';
-  yearsToShow: string[] = ['2021','2020','2019','2018','2017','2016','2015','2014','2013','2012','2011','2010'];
+  yearsToShow: string[] = ['2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010'];
+  yearlyAwardsTitle: string = '';
+  selectedYear: string = '';
 
   constructor(private activatedRoute: ActivatedRoute, private dataService: DataService) { }
 
@@ -21,11 +23,22 @@ export class AwardsComponent implements OnInit {
         .subscribe(data => {
           this.yearlyAwardsData = { ...data };
           this.active = "A Grade";
+          this.selectedYear = params['awardsYear'];
+          this.updateTitle();
         });
     });
   }
 
   updateUrl(event: any) {
     event.target.src = "/assets/img/players/NoImage.jpg";
+  }
+
+  changeGrade(grade: string) {
+    this.active = grade;
+    this.updateTitle();
+  }
+
+  updateTitle() {
+    this.yearlyAwardsTitle = this.selectedYear + ' ' + this.active + ' Awards';
   }
 }
--- src/app/totd/totd.component.ts
+++ src/app/totd/totd.component.ts
@@ -8,6 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TeamOfTheDecadeComponent implements OnInit {
   totdImageSource: string = '';
+  totdDropdownText: string = '';
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
@@ -15,12 +16,16 @@ export class TeamOfTheDecadeComponent implements OnInit {
     this.activatedRoute.params.subscribe(params => {
       if (params['totdName'] == '2010-2019-Senior') {
         this.totdImageSource = '2010 - 2019 TOTD - Senior.png';
+        this.totdDropdownText = 'Senior - 2010 to 2019';
       } else if (params['totdName'] == '2010-2019-Junior') {
         this.totdImageSource = '2010 - 2019 TOTD - Junior.png';
+        this.totdDropdownText = 'Junior - 2010 to 2019';
       } else if (params['totdName'] == '2000-2009-Senior') {
         this.totdImageSource = '2000 - 2009 TOTD - Senior.png';
+        this.totdDropdownText = 'Senior - 2000 to 2009';
       } else if (params['totdName'] == '2000-2009-Junior') {
         this.totdImageSource = '2000 - 2009 TOTD - Junior.png';
+        this.totdDropdownText = 'Junior - 2000 to 2009';
       }
     });
   }
