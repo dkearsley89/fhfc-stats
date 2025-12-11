afadec4a4e9f94daec9327394b3543b09ccb9d39
dkearsley89 <34358556+dkearsley89@users.noreply.github.com>
2025-12-11 22:14:37 +1030

    2025 EOY Updates

--- src/app/awards/awards.component.ts
+++ src/app/awards/awards.component.ts
@@ -15,7 +15,7 @@ import { NgbNavModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 export class AwardsComponent implements OnInit {
   yearlyAwardsData!: YearlyAwards;
   active: string = '';
-  yearsToShow: string[] = ['2024','2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010'];
+  yearsToShow: string[] = ['2025','2024','2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010'];
   yearlyAwardsTitle: string = '';
   selectedYear: string = '';
 
